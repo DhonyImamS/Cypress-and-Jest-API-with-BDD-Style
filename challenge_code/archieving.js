5b77fd54c104acd86eea4b6ffe4d71810efd6cea
DhonyImamSaputra <dhony.imamsaputra@larisin.id>
2020-08-06 11:48:52 +0700

    answer archieved javascript files

---
+++ challenge_code/archieving.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const archiver = require('archiver');
+
+async function archieveService(config, extensitonFile) {
+        const {
+            mode, optionsCompress, fileArchievedName
+        } = config;
+        const output = fs.createWriteStream('./archieve/' + fileArchievedName);
+        const archive = archiver(mode, optionsCompress);
+
+        output.on('close', function() {
+            console.log(archive.pointer() + ' total bytes');
+            console.log('ARCHIEVE SUCCESSFULL PLEASE LOOK FOR FILE UNDER FOLDER ARCHIEVE');
+        });
+
+        archive.glob(`files/directoryA/*.${extensitonFile}`);
+        archive.glob(`files/directoryB/*.${extensitonFile}`);
+        archive.glob(`files/directoryC/*.${extensitonFile}`);
+
+        archive.pipe(output);
+        archive.finalize();
+}
+
+async function archieving(archieveMode, targetExtention) {
+    let config;
+
+    switch (archieveMode) {
+        case 'zip' :
+            config = {
+                mode: 'zip',
+                optionsCompress: { zlibs: { level: 9 }},
+                fileArchievedName: 'archieve.zip'
+            };
+            break;
+
+        case 'tar' :
+            config = {
+                mode: 'tar',
+                optionsCompress: { gzipOptions: { level: 9 }},
+                fileArchievedName: 'archieve.tgz'
+            };
+            break;
+
+        default:
+            config = {
+                mode: 'unsupported feature archieving'
+            };
+            break;
+    } 
+
+    if (config.mode !== 'unsupported feature archieving') {
+        await archieveService(config, targetExtention);
+    } else {
+        console.log('Currently Only zip and tar mode archieve');
+    }
+    
+};
+
+archieving('zip','txt');
