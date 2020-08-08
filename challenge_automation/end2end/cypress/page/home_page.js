de70e849e5780d3eb87c99abdabaf3e0fb69c54e
DhonyImamSaputra <dhony.imamsaputra@larisin.id>
2020-08-09 01:33:50 +0700

    WOHO AddToCart Success & use  Conditional Test

--- challenge_automation/end2end/cypress/page/home_page.js
+++ challenge_automation/end2end/cypress/page/home_page.js
@@ -3,6 +3,12 @@ class HomePage {
     static navigatePage(urls) {
         cy.viewport(1440, 900) 
         cy.visit(`${Cypress.config().baseUrl}${urls}`);
+
+        cy.get('#desktopBannerWrapped button:nth-child(1)').then(($element) => {
+            if ($element.is(":visible")) {
+                $element.click();
+            }}
+        )
     }
 
     static clickPromoTab() {
@@ -10,12 +16,19 @@ class HomePage {
     }
 
     static searchProduk(produkName) {
-        cy.get('img[alt="magnifier icon"]').click();
-        cy.get('input[placeHolder="Cari produk"]').click().focused().clear().type(produkName).type('{enter}');
+        cy.get('body').find('img[alt="magnifier icon"]').should('be.visible').click();
+        cy.get('body').find('input[placeHolder="Cari produk"]').should('be.visible').click().focused().clear().type(produkName).type('{enter}');
+        cy.get('div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2 > div').should('have.length.gt', 0);
+        
+        cy.get('#desktopBannerWrapped button:nth-child(1)').then(($element) => {
+            if ($element.is(":visible")) {
+                $element.click();
+            }}
+        )
     }
 
     static findProductFromList(produkName) {
-        cy.get('body').find('section.css-1eanzts>.css-1ud1l9d').each(async ($el, index) => {
+        cy.get('body').find('section.css-1eanzts>.css-1ud1l9d').should('be.visible').each(async ($el, index) => {
             cy.wrap($el).invoke('text').then((text => {
                 if(text.trim() === produkName) {
                     cy.get(`div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2 > div:nth-child(${index+1}) > section[data-test="product"] a.no-highlight img`).click();
@@ -26,7 +39,7 @@ class HomePage {
     }
 
     static findSingleProduct() {
-        cy.get(`div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2 > div:nth-child(1) > section[data-test="product"] a.no-highlight img`).click();
+        cy.get('body').find(`div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2 > div:nth-child(1) > section[data-test="product"] a.no-highlight img`).should('exist').should('be.visible').click();
     }
 
     static assertionText(textInput) {
--- challenge_automation/end2end/cypress/page/product_page.js
+++ challenge_automation/end2end/cypress/page/product_page.js
@@ -5,8 +5,20 @@ class ProductPage {
     }
 
     static tambahKeTroli() {
-        cy.get('button').find('#addToCart .MuiButton-label').click();
-        cy.screenshot();
+        cy.server();
+        cy.route('https://qa.fabelio.com/rest/default/V1/customer/summary').as('getUsersummary');
+        cy.route('https://qa.fabelio.com/rest/default/V1/menu/topmenu').as('getTopMenu');
+
+        cy.get('#desktopBannerWrapped button:nth-child(1)').then(($element) => {
+            if ($element.is(":visible")) {
+                $element.click();
+            }}
+        )
+        
+        cy.get('button[type="button"][id="addToCart"]').should('have.text', 'TAMBAH KE TROLI');
+        cy.get('button[type="button"][id="addToCart"]').find('span.MuiButton-startIcon.MuiButton-iconSizeLarge img').then(($elm) => {
+            $elm.click();
+        });
     }
 
 }
